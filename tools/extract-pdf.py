af3fa244b39aef0e5ca609d5219693b7c4c49bbc
ShadowGlimpse <allthegamesilove@gmail.com>
2020-02-26 18:40:42 +0200

    First commit, pdf extraction

---
+++ tools/extract-pdf.py
@@ -0,0 +1,30 @@
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+import io
+
+
+def pdfparser(data):
+    fp = open(data, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data = retstr.getvalue()
+
+    print(data)
+
+
+pdfparser('/home/Serelia/Uni/Ptixiaki/pdf_files/9ΘΑΤ4653ΠΓ-9Ξ1.pdf')
+
+# if __name__ == '__main__':
+#     pdfparser(sys.argv[1])
