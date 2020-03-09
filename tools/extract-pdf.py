470e551f431a6b24e21d4a998d55622af7080eea
ShadowGlimpse <allthegamesilove@gmail.com>
2020-03-10 01:48:22 +0200

    Added json parser for url, and changed pdf parser to work with url instead of local pdf)

--- tools/extract-pdf.py
+++ tools/extract-pdf.py
@@ -1,38 +1,56 @@
 import sys
 
+import requests
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.layout import LAParams
 import io
 import json
-import dload
-
+from urllib.request import urlopen
 
 def pdfparser(data):
-    fp = open(data, 'rb')
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
     codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    # Create a PDF interpreter object.
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    # Process each page contained in the document.
+    response = requests.get(data)
+    with io.BytesIO(response.content) as open_pdf_file:
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
 
-    for page in PDFPage.get_pages(fp):
-        interpreter.process_page(page)
-        data = retstr.getvalue()
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        # Create a PDF interpreter object.
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # Process each page contained in the document.
 
-    print(data)
+        for page in PDFPage.get_pages(open_pdf_file):
+            interpreter.process_page(page)
+            data = retstr.getvalue()
+
+        print(data)
+
+
+def jsontodocs(url):
+    data = requests.get(url)
+    jsonobj = json.loads(data.text)
+    # jstring=json.dumps(obj, indent=4, ensure_ascii=False).encode('utf8')
+    # print(jstring.decode())
+    doclist = []
+    # json in gov link is a json object, that contains a json array
+    # which in turn contains each object with the iformation needed
+    # as well as the document url
+    inside_jsonobj = jsonobj['decisionResultList']
+    for j in inside_jsonobj:
+        doclist.append(j['documentUrl'])
+    return doclist
+
 
 agrUrl = "https://www.diavgeia.gov.gr/luminapi/api/search/export?q=organizationUid:%22100010899%22&sort=recent&wt=json"
-agrjsonurl = dload.json(agrUrl)
-# agrJson = json.load(agrUrl)
-print(agrjsonurl)
-
 docUrl = 'https://diavgeia.gov.gr/doc/ΩΕΚ64653ΠΓ-2ΞΡ'
-# pdfparser('https://diavgeia.gov.gr/doc/ΩΕΚ64653ΠΓ-2ΞΡ')
+
+doclist = jsontodocs(agrUrl)
+pdfparser(doclist[5])
+
+# pdfparser(docUrl)
 
 # if __name__ == '__main__':
 #     pdfparser(sys.argv[1])
